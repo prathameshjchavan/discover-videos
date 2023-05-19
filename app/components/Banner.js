b250cf6e10e8f8d850cf9598b433fe73fe2eca3e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-05-19 11:27:56 +0530

    styling Banner component

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,16 +1,31 @@
 "use client";
 
+import styles from "./Banner.module.css";
+
 const Banner = ({ title, subTitle, imgUrl }) => {
 	const handleOnPlay = () => {
 		console.log("handleOnPlay");
 	};
 
 	return (
-		<div>
-			<h3>{title}</h3>
-			<h3>{subTitle}</h3>
-			<button onClick={handleOnPlay}>Play</button>
+		<div className={styles.container}>
+			<div className={styles.leftWrapper}>
+				<div className={styles.left}>
+					<div className={styles.nseriesWrapper}>
+						<p className={styles.firstLetter}>N</p>
+						<p className={styles.series}>S E R I E S</p>
+					</div>
+					<h3 className={styles.title}>{title}</h3>
+					<h3 className={styles.subTitle}>{subTitle}</h3>
+					<div className={styles.playBtnWrapper}>
+						<button className={styles.btnWithIcon} onClick={handleOnPlay}>
+							<span className={styles.playText}>Play</span>
+						</button>
+					</div>
+				</div>
+			</div>
 			<div
+				className={styles.bannerImg}
 				style={{
 					backgroundImage: `url('${imgUrl}')`,
 					width: "100%",
