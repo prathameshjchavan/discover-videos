58cb7709cf2ef7811a5897274d74f7a6cb1ed5a1
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-05-19 11:16:47 +0530

    building Banner component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import Banner from "./components/Banner";
+
 export default function Home() {
 	return (
 		<main>
 			<h1>Netflix</h1>
+
+			<Banner
+				title="Clifford the red dog"
+				subTitle="a very cute dog"
+				imgUrl="/static/clifford.webp"
+			/>
 		</main>
 	);
 }
